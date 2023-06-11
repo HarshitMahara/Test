15d82151b55112dc5321bba7cdc8d69bb3708ac3
Pushkar Kumar <127659680+GoodGuyPJ@users.noreply.github.com>
2023-06-11 13:55:46 +0530

    Update tempconvertor.java

--- JAVA/tempconv.java
+++ JAVA/tempconv.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter temperature in degree Celsius: ");
+        double celsius = scanner.nextDouble();
+        // Conversion to Fahrenheit
+        double fahrenheit = (celsius * 9/5) + 32; 
+        // Conversion to Kelvin
+        double kelvin = celsius + 273.15; 
+        
+        System.out.println(celsius + " degree Celsius is equal to " + fahrenheit + " degree Fahrenheit.");
+        System.out.println(celsius + " degree Celsius is equal to " + kelvin + " Kelvin.");
+        
+        scanner.close();
+    }
+}
